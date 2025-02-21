50e105272bdcdc481533fe05c0638ee612d63058
TazwoarCommits <tazwoarlives@gmail.com>
2025-02-21 22:13:10 +0600

    in progress

--- src/Firebase/firebase.config.js
+++ src/Firebase/firebase.config.js
@@ -14,13 +14,6 @@ const firebaseConfig = {
   appId: import.meta.env.VITE_appId,
 };
 
-// apiKey: "AIzaSyCIWu4XGW-FJMJT2fFAqI-i6lpAIfrkm1Y",
-// authDomain: "task-todo-3cc35.firebaseapp.com",
-// projectId: "task-todo-3cc35",
-// storageBucket: "task-todo-3cc35.firebasestorage.app",
-// messagingSenderId: "954706845274",
-// appId: "1:954706845274:web:0cca36f1342af738040263"
-
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
